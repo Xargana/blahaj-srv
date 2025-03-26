c96af6476a1d2e65eb92b90483efc9d1d62df086
Xargana <mertarabaci@gmail.com>
2025-03-26 18:17:49 +0300

    a2

--- status/server.js
+++ status/server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = 2589;
 const REMOTE_SERVER = "https://example.com"; // Change this
 
-const CHECK_INTERVAL = 30 * 1000;
+const CHECK_INTERVAL = 5 * 1000;
 
 let serverStatus = {
     online: false,
