550a6f6c3fd09d474f361420780fa699c93fdfcb
root <root@blahaj.tr>
2025-03-29 09:36:57 +0000

    auto update

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /var/server/blahaj-srv
 git commit -a -m "auto update"
-git pull origin main -m "auto merge"
+git pull origin main
 chown -R www-data:www-data /var/server/blahaj-srv 
 pm2 restart api_status-server
